db3fc96080dba929764d35dd123921d458f4b60b
MyLittleKnight <muhammed-e.72@hotmail.com>
2017-11-07 22:36:30 +0300

    simple ball game
    its funny gam

---
+++ simpleGame/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace simpleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.slide = new System.Windows.Forms.PictureBox();
+            this.vert = new System.Windows.Forms.Timer(this.components);
+            this.horz = new System.Windows.Forms.Timer(this.components);
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.keyWatch = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(0, 1);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(146, 120);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // slide
+            // 
+            this.slide.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.slide.Image = ((System.Drawing.Image)(resources.GetObject("slide.Image")));
+            this.slide.Location = new System.Drawing.Point(170, 529);
+            this.slide.Name = "slide";
+            this.slide.Size = new System.Drawing.Size(340, 24);
+            this.slide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.slide.TabIndex = 2;
+            this.slide.TabStop = false;
+            // 
+            // vert
+            // 
+            this.vert.Enabled = true;
+            this.vert.Interval = 10;
+            this.vert.Tick += new System.EventHandler(this.vert_Tick);
+            // 
+            // horz
+            // 
+            this.horz.Enabled = true;
+            this.horz.Interval = 10;
+            this.horz.Tick += new System.EventHandler(this.horz_Tick);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(978, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // keyWatch
+            // 
+            this.keyWatch.Enabled = true;
+            this.keyWatch.Interval = 10;
+            this.keyWatch.Tick += new System.EventHandler(this.keyWatch_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(296, 423);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(734, 130);
+            this.panel1.TabIndex = 4;
+            this.panel1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(686, 107);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Pause";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1031, 555);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.slide);
+            this.Controls.Add(this.ball);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox slide;
+        private System.Windows.Forms.Timer vert;
+        private System.Windows.Forms.Timer horz;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer keyWatch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ simpleGame/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simpleGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        int speed = 3;
+        int slide_speed = 10;
+        int vdir = +1;
+        bool game_ower = false;
+        private void vert_Tick(object sender, EventArgs e)
+        {
+             if(ball.Top < 0)
+             {
+                 vdir = 1;
+             }
+             else if(ball.Top > (this.Height - ball.Height -slide.Height))
+             {
+                 //lets add game logic
+                 if(ball.Left < slide.Left || (ball.Left+ball.Width)>(slide.Left + slide.Width))
+                 {
+                     vert.Enabled = horz.Enabled = false;
+                     MessageBox.Show("Hadeee oldumu şimdi!!!");
+                 }
+
+
+
+
+                 vdir = -1;
+             }
+            
+                 ball.Top += (speed*vdir);
+
+             
+        }
+
+        int hdir = +1;
+        private void horz_Tick(object sender, EventArgs e)
+        {
+            if (ball.Left < 0)
+            {
+                hdir = +1;
+            }
+            else if (ball.Left > (this.Width - ball.Width))
+            {
+                hdir = -1;
+            }
+            
+            ball.Left += (speed*hdir);
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+       //for slider movement.
+        bool _left  = false;
+        bool _right = false;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //active 
+            if (e.KeyCode == Keys.Right)
+            {
+                _right = true;
+            }
+            else if(e.KeyCode == Keys.Left)
+            {
+                _left = true;
+            }
+            else if(e.KeyCode == Keys.Space)
+            {
+                //pause game
+                vert.Enabled = horz.Enabled = false;
+                panel1.Visible = true;
+            }
+            else if (e.KeyCode == Keys.Enter && vert.Enabled== false)
+            {
+                vert.Enabled = horz.Enabled = true;
+                panel1.Visible = false;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //inactive
+            if(e.KeyCode == Keys.Right || e.KeyCode == Keys.Left)
+            {
+                _left = false; 
+                _right = false;//disable all
+            }
+        }
+
+        private void keyWatch_Tick(object sender, EventArgs e)
+        {
+            if(_right && slide.Left < (this.Width-slide.Width))
+            {
+                slide.Left += 10; 
+            }
+            else if ( _left  && slide.Left > 0)
+            {
+                slide.Left -= 10;
+            }
+        }
+
+    }
+}
